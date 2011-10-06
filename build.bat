e5edca1474423fd35dd4ff3a9a708e6015b7e17a
Stuart Ashworth <stuart@stuartashworth.com>
2011-10-06 15:29:26 +0100

    Added separate JSB files for each plugin.

--- build.bat
+++ build.bat
@@ -1,12 +1,10 @@
 cd Builder
 
 
-java -jar JSBuilder2.jar -v -p D:\Workspace\Ext.ux.Calendar\Builder\Ext.ux.Calendar.jsb2 -d D:\Workspace\Ext.ux.Calendar
+java -jar JSBuilder2.jar -v -p C:\www\Ext.ux.Calendar\Builder\Ext.ux.Calendar.jsb2 -d C:\www\Ext.ux.Calendar
 
-cd..
+java -jar JSBuilder2.jar -v -p C:\www\Ext.ux.Calendar\Builder\Ext.ux.CalendarSimpleEvents.jsb2 -d C:\www\Ext.ux.Calendar
 
-cd..
-
-jsduck-3.0.pre.exe Ext.ux.Calendar --output docs/ --ignore-global --verbose --title "Ext.ux.Calendar Documentation" --footer "Generated with JSDuck"
+java -jar JSBuilder2.jar -v -p C:\www\Ext.ux.Calendar\Builder\Ext.ux.CalendarEvents.jsb2 -d C:\www\Ext.ux.Calendar
 
 PAUSE
