29b88fc311073af2913e48d08d5c4db08479b0ff
Stuart Ashworth <stuart@stuartashworth.com>
2012-01-04 21:18:17 +0000

    Updated built.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,8 @@
 cd Builder
 
-java -jar JSBuilder2.jar -v -p D:\workspace\Ext.ux.TouchCalendar\builder\Ext.ux.TouchCalendarEvents.jsb2 -d D:\workspace\Ext.ux.TouchCalendar
+java -jar JSBuilder2.jar -v -p D:\workspace\Ext.ux.TouchCalendar\builder\Ext.ux.TouchCalendar.jsb2 -d D:\workspace\Ext.ux.TouchCalendar
+
+java -jar JSBuilder2.jar -v -p D:\workspace\Ext.ux.TouchCalendar\builder\Ext.ux.TouchCalendarView.jsb2 -d D:\workspace\Ext.ux.TouchCalendar
 
 java -jar JSBuilder2.jar -v -p D:\workspace\Ext.ux.TouchCalendar\builder\Ext.ux.TouchCalendarSimpleEvents.jsb2 -d D:\workspace\Ext.ux.TouchCalendar
 
