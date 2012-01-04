5682433676e0146b0e99d1c40cc92609bf4a1bb2
Stuart Ashworth <stuart@stuartashworth.com>
2012-01-04 09:38:35 +0000

    Updated build file with new paths.

--- build.bat
+++ build.bat
@@ -1,16 +1,15 @@
 cd Builder
 
+java -jar JSBuilder2.jar -v -p D:\workspace\Ext.ux.TouchCalendar\builder\Ext.ux.TouchCalendarEvents.jsb2 -d D:\workspace\Ext.ux.TouchCalendar
 
-java -jar JSBuilder2.jar -v -p c:\www\Ext.ux.TouchCalendar\Builder\Ext.ux.TouchCalendar.jsb2 -d c:\www\
+java -jar JSBuilder2.jar -v -p D:\workspace\Ext.ux.TouchCalendar\builder\Ext.ux.TouchCalendarSimpleEvents.jsb2 -d D:\workspace\Ext.ux.TouchCalendar
 
-java -jar JSBuilder2.jar -v -p c:\www\Ext.ux.TouchCalendar\Builder\Ext.ux.TouchCalendarSimpleEvents.jsb2 -d c:\www\Ext.ux.Calendar
-
-java -jar JSBuilder2.jar -v -p c:\www\Ext.ux.TouchCalendar\Builder\Ext.ux.TouchCalendarEvents.jsb2 -d c:\www\Ext.ux.Calendar
-
-cd..
+java -jar JSBuilder2.jar -v -p D:\workspace\Ext.ux.TouchCalendar\builder\Ext.ux.TouchCalendarEvents.jsb2 -d D:\workspace\Ext.ux.TouchCalendar
 
 cd..
 
-jsduck-3.0.pre.exe Ext.ux.Calendar --output Ext.ux.TouchCalendar/docs --ignore-global --title "Ext.ux.TouchCalendar Documentation" --footer "Generated with JSDuck"
+cd..
+
+jsduck-3.0.pre.exe Ext.ux.TouchCalendar --output Ext.ux.TouchCalendar/docs --ignore-global --title "Ext.ux.TouchCalendar Documentation" --footer "Generated with JSDuck"
 
 PAUSE
