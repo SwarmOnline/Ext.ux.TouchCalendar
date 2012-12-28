93c9212c2f3d54ac99330de3e1f3a2f872fb9418
Stuart Ashworth <stuart@stuartashworth.com>
2012-12-28 16:13:44 +1100

    Upgraded to ST2.1.0

--- lib/sencha/resources/themes/compass_init.rb
+++ lib/sencha/resources/themes/compass_init.rb
@@ -3,4 +3,8 @@
 dir = File.dirname(__FILE__)
 require File.join(dir, 'lib', 'theme_images.rb')
 
+# Include compass-recipes
+require File.join(File.dirname(__FILE__), 'vendor', 'compass-recipes', 'config')
+
+Compass::BrowserSupport.add_support('repeating-linear-gradient', 'webkit', 'moz', 'o', 'ms')
 Compass::Frameworks.register 'sencha-touch', dir
